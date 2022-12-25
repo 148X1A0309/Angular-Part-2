c2618146d3b1513db6eb15a4ce020c4cbc06d51e
148X1A0309 <90388113+148X1A0309@users.noreply.github.com>
2022-12-25 22:43:32 +0530

    2 files modified, 3 added

---
+++ src/app/Directives/structural-directives/structural-directives.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-structural-directives',
+  templateUrl: './structural-directives.component.html',
+  styleUrls: ['./structural-directives.component.css']
+})
+export class StructuralDirectivesComponent implements OnInit {
+showDetails=false;
+log=[];
+  constructor() { }
+
+  ngOnInit() {
+  }
+  OnToggleData(){
+    this.showDetails=!this.showDetails
+    this.log.push(this.log.length+1);
+    }
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,11 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { StructuralDirectivesComponent } from './Directives/structural-directives/structural-directives.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  declarations: [ AppComponent, HelloComponent,StructuralDirectivesComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
