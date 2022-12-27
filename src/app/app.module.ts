5a1664725a655366b55bc8671c88695e82e3325d
148X1A0309 <90388113+148X1A0309@users.noreply.github.com>
2022-12-27 08:47:48 +0530

    3 files modified, 2 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,11 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { StructuralDirectivesComponent } from './Directives/structural-directives/structural-directives.component';
+import { HeaderComponent } from './NewApp/header/header.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent,StructuralDirectivesComponent ],
+  declarations: [ AppComponent, HelloComponent,StructuralDirectivesComponent ,HeaderComponent],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
