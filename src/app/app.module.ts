b2cd39370c9efe2111aac2004757beb4973f9fb7
148X1A0309 <90388113+148X1A0309@users.noreply.github.com>
2022-12-27 08:53:18 +0530

    1 file modified, 6 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,13 @@ import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { StructuralDirectivesComponent } from './Directives/structural-directives/structural-directives.component';
 import { HeaderComponent } from './NewApp/header/header.component';
+import { RecipeDetailsComponent } from './NewApp/recipes/recipe-details/recipe-details.component';
+import { RecipeItemComponent } from './NewApp/recipes/recipes-list/recipe-item/recipe-item.component';
+import { RecipesListComponent } from './NewApp/recipes/recipes-list/recipes-list.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent,StructuralDirectivesComponent ,HeaderComponent],
+  declarations: [ AppComponent, HelloComponent,StructuralDirectivesComponent ,HeaderComponent,RecipeDetailsComponent,RecipeItemComponent,RecipesListComponent],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
